0ee36f5ca009f8737d458016ab1f9af8096de353
T_cat9000_2 <terpstragraham@gmail.com>
2025-01-24 13:55:29 -0800

    update the kernel

--- krnl.py
+++ krnl.py
@@ -1,10 +1,5 @@
 # the system kernel.
 
-import os
-import sys
+from .plankcore import Bootmgr
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-if script_dir not in sys.path:
-    sys.path.append(script_dir)
-
-from plankcore import Critical
+Bootmgr.Boot()

ae48f7aedb2a0fcb3f57d2fe23a5a0ec866abfe4
T_cat9000_2 <terpstragraham@gmail.com>
2025-01-24 14:01:15 -0800

    use imp

--- interface.py
+++ interface.py
@@ -1,4 +1,15 @@
-from plankcore import Critical, Utils
+import imp
+import os
+
+plankcore_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "plankcore.py")
+
+if os.path.exists(plankcore_path):
+    plankcore = imp.load_source('plankcore', plankcore_path)
+else:
+    print("plankcore.py not found!")
+
+Critical = plankcore.Critical
+Utils = plankcore.Utils
 
 class Cli:
     def Loop():

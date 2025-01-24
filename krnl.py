b81fe285b075c2a7ca5822c7082e83b9643fe011
T_cat9000_2 <terpstragraham@gmail.com>
2025-01-24 14:11:41 -0800

    use importlib

--- krnl.py
+++ krnl.py
@@ -1,15 +1,16 @@
 # The system kernel
 
-import imp
+import importlib.util
 import os
 
 plankcore_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "plankcore.py")
 
 if os.path.exists(plankcore_path):
-    plankcore = imp.load_source('plankcore', plankcore_path)
+    spec = importlib.util.spec_from_file_location("plankcore", plankcore_path)
+    plankcore = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(plankcore)
+    Bootmgr = plankcore.Bootmgr
 else:
     print("plankcore.py not found!")
-
-Bootmgr = plankcore.Bootmgr
 
 Bootmgr.Boot()

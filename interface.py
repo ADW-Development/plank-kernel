ca0768b6c4fa556ca0b1d7d43334a76a2dd12110
T_cat9000_2 <terpstragraham@gmail.com>
2025-01-24 14:42:50 -0800

    update and bug fixes

--- interface.py
+++ interface.py
@@ -1,45 +1,58 @@
 import importlib.util
 import os
+import time
+import sys
 
-plankcore_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "plankcore.py")
+def load_core():
+    global Critical
+    global Utils
 
-if os.path.exists(plankcore_path):
-    spec = importlib.util.spec_from_file_location("plankcore", plankcore_path)
-    plankcore = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(plankcore)
-    Critical = plankcore.Critical
-    Utils = plankcore.Utils
-else:
-    print("plankcore.py not found!")
+    plankcore_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "plankcore.py")
+
+    if os.path.exists(plankcore_path):
+        spec = importlib.util.spec_from_file_location("plankcore", plankcore_path)
+        plankcore = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(plankcore)
+        Critical = plankcore.Critical
+        Utils = plankcore.Utils
+    else:
+        print("plankcore.py not found!")
 
 class Cli:
     def Loop():
-        Cli.Input()
-        Cli.Processcommand()
+        while True:
+            Cli.Input()
+            Cli.Processcommand()
     
     def Input():
         global Command
-        Command = input("Plank$>")
+        Command = input("Plank$> ")
     
     def Processcommand():
-        if Command == "help":
-            Commands.help()
+        if Command == "Help":
+            Commands.Help()
         
-        if Command == "info":
-            Commands.info()
+        if Command == "Info":
+            Commands.Info()
         
-        if Command == "Force_Panic":
+        if Command == "Force Panic":
             Commands.Force_Panic()
 
 class Commands:
-    def help():
-        Critical.Outp("help")
-        Critical.Outp("info")
+    def Help():
+        load_core()
+        Critical.Outp("Help")
+        Critical.Outp("Info")
+        Critical.Outp("Force Panic")
     
-    def info():
+    def Info():
+        load_core()
         Critical.Outp("System: Plank kernel")
         Critical.Outp("Software Manufacturer: ADW Systems")
-        Critical.Outp("Version: Beta 0.1.0")
+        Critical.Outp("Version: Beta 0.1.2")
     
     def Force_Panic():
+        load_core()
         Utils.Panic("User_Forced_Panic")
+        time.sleep(3)
+        sys.exit()

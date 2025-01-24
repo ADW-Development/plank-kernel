867d5cb0902dce082f2ab9fe381bbaa8d239e52a
T_cat9000_2 <terpstragraham@gmail.com>
2025-01-24 14:10:35 -0800

    use importlib

--- interface.py
+++ interface.py
@@ -1,15 +1,16 @@
-import imp
+import importlib.util
 import os
 
 plankcore_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "plankcore.py")
 
 if os.path.exists(plankcore_path):
-    plankcore = imp.load_source('plankcore', plankcore_path)
+    spec = importlib.util.spec_from_file_location("plankcore", plankcore_path)
+    plankcore = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(plankcore)
+    Critical = plankcore.Critical
+    Utils = plankcore.Utils
 else:
     print("plankcore.py not found!")
-
-Critical = plankcore.Critical
-Utils = plankcore.Utils
 
 class Cli:
     def Loop():

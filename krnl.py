b36ae4cbb3020e6fe339c150027a896a2a3f7500
T_cat9000_2 <terpstragraham@gmail.com>
2025-01-24 14:02:34 -0800

    use imp

--- krnl.py
+++ krnl.py
@@ -1,5 +1,15 @@
-# the system kernel.
+# The system kernel
 
-from plankcore import Bootmgr
+import imp
+import os
+
+plankcore_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "plankcore.py")
+
+if os.path.exists(plankcore_path):
+    plankcore = imp.load_source('plankcore', plankcore_path)
+else:
+    print("plankcore.py not found!")
+
+Bootmgr = plankcore.Bootmgr
 
 Bootmgr.Boot()

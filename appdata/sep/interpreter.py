f86ed5717ff8304a66c1c5c3010258a59393468b
T_cat9000_2 <terpstragraham@gmail.com>
2025-03-30 14:32:59 -0700

    update

--- appdata/sep/interpreter.py
+++ appdata/sep/interpreter.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import re
 import os
 import sys
@@ -5,21 +7,6 @@ import sys
 InpProgram = sys.argv[1]
 program = os.path.abspath(os.path.join("programs", f"{InpProgram}.psep"))
 lines = []
+variables = {}
 
-with open(program, "r") as file:
-    for line in file:
-        lines.append(line.strip())
-
-processedlines = []
-
-for line in lines:
-    line = re.sub(r'2(?=[a-zA-Z])', '', line)
-    line = re.sub(r'\b2\b', ' ', line)
-    processedlines.append(line)
-
-out = ''
-
-for char in processedlines:
-    out += char
-
-print(out)
+# new code to be added

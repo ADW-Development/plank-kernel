010240fe6e0e289ff0cf51b92f0980fa468eb1b6
T_cat9000_2 <terpstragraham@gmail.com>
2025-06-21 13:30:11 -0700

    update

--- appdata/sep/interpreter.py
+++ appdata/sep/interpreter.py
@@ -9,4 +9,20 @@ program = os.path.abspath(os.path.join("programs", f"{InpProgram}.psep"))
 lines = []
 variables = {}
 
-# new code to be added
+with open(program, "r") as file:
+    for line in file:
+        lines.append(line.strip())
+
+processedlines = []
+
+for line in lines:
+    line = re.sub(r'2(?=[a-zA-Z])', '', line)
+    line = re.sub(r'\b2\b', ' ', line)
+    processedlines.append(line)
+
+out = ''
+
+for char in processedlines:
+    out += char
+
+print(out)
